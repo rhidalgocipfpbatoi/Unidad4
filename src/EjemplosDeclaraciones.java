b571afff97fee2727150f6de6ff3836fa78744bd
rhidalgocipfpbatoi <rhidalgo@cipfpbatoi.es>
2020-11-12 15:48:56 +0100

    Ejemplo leer entero con métodos

---
+++ src/EjemplosDeclaraciones.java
@@ -0,0 +1,38 @@
+public class EjemplosDeclaraciones {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * El máximo de tres números
+     */
+    public int maximoTresNumeros(int numero1, int numero2, int numero3) {
+        return 0;
+    }
+
+    /**
+     * La media aritmética de tres números reales
+     */
+    public double mediaTresNumeros(double numero1, double numero2, double numero3){
+        return 0.;
+    }
+
+    /**
+     * Dada una nota, transformala a texto
+     */
+
+    public String notaATexto(int nota) {
+        return null;
+    }
+
+    /**
+     * Saber los días que tiene un mes
+     */
+
+    public int diasDeMes(String nombreMes) {
+        return 0;
+    }
+
+
+}
