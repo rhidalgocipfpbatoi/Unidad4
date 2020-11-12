296a1d5f5c2efb2fe4f14933656b018220eb9741
rhidalgocipfpbatoi <rhidalgo@cipfpbatoi.es>
2020-11-12 15:48:23 +0100

    Ejemplo leer entero con métodos

---
+++ src/ConversionesExplicitas.java
@@ -0,0 +1,15 @@
+public class ConversionesExplicitas {
+
+    public static void main(String[] args) {
+
+        double numeroReal = 3.6;
+        int parteEntera = (int) numeroReal; // Conversión explícita o casting
+
+        int numeroEntero = 4;
+        double enteroAReal = numeroEntero; // Conversión implicita
+
+
+        // double, float: --> tipos reales
+        // long, int, short, byte: --> tipos enteros
+    }
+}
